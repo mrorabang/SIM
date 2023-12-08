52b0d0fbb3784ee65f90cdde48de65dda4515852
Minh Quan <150663837+mrorabang@users.noreply.github.com>
2023-12-08 12:52:43 +0700

    chup mh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import{Routes,Route,Link} from 'react-router-dom'
 import "./App.css";
 import Home from "./Home";
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -9,107 +9,134 @@ import {
   MDBCol,
   MDBInput,
 } from "mdb-react-ui-kit";
+import html2canvas from 'html2canvas';
 
 const Home = () => {
-    const [inputText, setInputText] = useState("");
-    const [output, setOutput] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const originalInputText = useRef("");
-    const parseInput = () => {
-      const regex = /([\d.]+)=(.+)/g;
-      const matches = [...inputText.matchAll(regex)];
-  
-      if (!matches || matches.length === 0) {
-        console.log("Không có sản phẩm nào được tìm thấy.");
-        return;
-      }
-  
-      const result = matches.map((match) => {
-        const name = match[1];
-        const price = parseFloat(match[2]);
-        return { name, price };
-      });
-  
-      setOutput(result);
-      originalInputText.current = inputText;
-    };
-  
-    const handleSearch = () => {
-      const filteredResults = output.filter((product) =>
-        product.name.includes(searchTerm)
-      );
-      setOutput(filteredResults);
-    };
-  
-    useEffect(() => {
-      handleSearch();
-    }, [searchTerm]);
-  
-    
-    return (
-      <div className="App">
-        <label htmlFor="inputText">Hãy nhập mảng tại đây:</label>
-        <textarea
-          id="inputText"
-          cols="30"
-          rows="5"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-        ></textarea>
-        <br />
-        <MDBBtn onClick={parseInput}>Tách số và Giá Bán</MDBBtn>
-        <hr />
-        <MDBContainer>
-          <MDBRow center>
-            <MDBCol size="4">
-              <div className="result">
-                <strong>Kết quả:</strong>
-                <MDBListGroup numbered style={{ maxWidth: "26rem" }} light>
-                  {output.map((o) => {
-                    return (
-                      <MDBListGroupItem className="d-flex justify-content-between align-items-center">
-                        <div className="ms-2 me-auto">
-                          <div className="fw-bold">Số: {o.name}</div>
-                        </div>
-                        <MDBBadge pill light>
-                          {o.price} Triệu
-                        </MDBBadge>
-                      </MDBListGroupItem>
-                    );
-                  })}
-                </MDBListGroup>
-              </div>
-            </MDBCol>
-            <MDBCol size="4">
-              <MDBInput
-                label="Tìm số trong kết quả"
-                id="formControlLg"
-                type="text"
-                size="lg"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-              />
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-  
-        <hr />
-        <h1>Tạo ảnh</h1>
-        {output.map((o) => {
-          return (
-            <>
-              <div>
-              <img src="./img/bg.jpg" className="img" width={'500px'} alt="" />
-              <div className="group">
-                <p className="name">{o.name}</p>
-              <p className="price">Giá : {o.price} Triệu</p>
-              </div>
-              </div>
-            </>
-          );
-        })}
-      </div>
-    );
+  const [inputText, setInputText] = useState("");
+  const [output, setOutput] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const originalInputText = useRef("");
+  const parseInput = () => {
+    const regex = /([\d.]+)=(.+)/g;
+    const matches = [...inputText.matchAll(regex)];
+
+    if (!matches || matches.length === 0) {
+      console.log("Không có sản phẩm nào được tìm thấy.");
+      return;
+    }
+
+    const result = matches.map((match) => {
+      const name = match[1];
+      const price = parseFloat(match[2]);
+      return { name, price };
+    });
+
+    setOutput(result);
+    originalInputText.current = inputText;
   };
-  
-  export default Home;
+
+  const handleSearch = () => {
+    const filteredResults = output.filter((product) =>
+      product.name.includes(searchTerm)
+    );
+    setOutput(filteredResults);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [searchTerm]);
+
+  const captureAndDownload = (index) => {
+    const container = document.getElementById(`imageContainer-${index}`);
+    html2canvas(container).then(function (canvas) {
+      const imgUrl = canvas.toDataURL('image/png');
+      const link = document.createElement('a');
+      link.href = imgUrl;
+      link.download = `captured_image_${index}.png`;
+      link.click();
+    });
+  };
+
+
+  return (
+    <div className="App">
+      <label htmlFor="inputText">Hãy nhập list tại đây:</label>
+      <textarea
+        id="inputText"
+        cols="30"
+        rows="5"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      ></textarea>
+      <br />
+      <MDBBtn onClick={parseInput}>Tách số và Giá Bán</MDBBtn>
+      <hr />
+      <MDBContainer>
+        <MDBRow center>
+          <MDBCol size="4">
+            <div className="result">
+              <strong>Kết quả:</strong>
+              <MDBListGroup numbered style={{ maxWidth: "26rem" }} light>
+                {output.map((o) => {
+                  return (
+                    <MDBListGroupItem className="d-flex justify-content-between align-items-center">
+                      <div className="ms-2 me-auto">
+                        <div className="fw-bold">{o.name}</div>
+                      </div>
+                      <MDBBadge pill light>
+                        {o.price} Triệu
+                      </MDBBadge>
+                    </MDBListGroupItem>
+                  );
+                })}
+              </MDBListGroup>
+            </div>
+          </MDBCol>
+          <MDBCol size="4">
+            <MDBInput
+              label="Tìm số trong kết quả"
+              id="formControlLg"
+              type="text"
+              size="lg"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+
+      <hr />
+      <h1>Tạo ảnh</h1>
+      {output.map((o,index) => {
+        return (
+          <>
+            {/* <div>
+              <label id="imageContainer">
+                <img src="./img/bg.jpg" className="img" width={'100%'} alt="" />
+                <div className="group">
+                  <p className="name">{o.name}</p>
+                  <p className="price">Giá : {o.price} Triệu</p>
+                </div>
+              </label>
+              <MDBBtn className="btn-down" onClick={captureAndDownload}>Tải xuống</MDBBtn>
+            </div> */}
+            <div>
+            <label className='bg-image' id={`imageContainer-${index}`}>
+              <img src='./img/bg.jpg'  alt='Sample' />
+              <div className='mask' >
+                <div className='d-flex justify-content-center align-items-center h-100'>
+                  <p className='text-red mb-0 name'>{o.name}</p>
+                  <p className="price">Giá:{o.price} Triệu</p>
+                </div>
+              </div>
+            </label>
+            <MDBBtn className="btn-down" onClick={() => captureAndDownload(index)}>Tải xuống</MDBBtn>
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
